7b9597bc8e6fec0d4db1660cfdfdd82cbf020a60
nunes-bruno-m <nunes.bruno.m@gmail.com>
2020-02-29 15:49:51 +0000

    Add GroupEventCalendar

---
+++ src/Feature/Groups/code/Controllers/GroupEventCalendarController.cs
@@ -0,0 +1,20 @@
+﻿using Feature.Groups.Repositories;
+using Sitecore.XA.Feature.Events.Repositories;
+using Sitecore.XA.Foundation.Mvc.Controllers;
+
+namespace Feature.Groups.Controllers
+{
+    public class GroupEventCalendarController : StandardController
+    {
+        public IGroupEventCalendarRepository GroupEventCalendarRepository { get; set; }
+        public GroupEventCalendarController()
+        {
+            GroupEventCalendarRepository = new GroupEventCalendarRepository();
+        }
+        
+        protected override object GetModel()
+        {
+            return (object)this.GroupEventCalendarRepository.GetModel();
+        }
+    }
+}
---
+++ src/Feature/Groups/code/Repositories/GroupEventCalendarRepository.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.XA.Feature.Events.Models;
+using System.Collections.Generic;
+
+namespace Feature.Groups.Repositories
+{
+    public class GroupEventCalendarRepository : Sitecore.XA.Feature.Events.Repositories.EventCalendarRepository, IGroupEventCalendarRepository
+    {
+        protected override CalendarSettings GetCalendarSettings()
+        {
+            CalendarSettings calendarSettings = base.GetCalendarSettings();
+
+            if (calendarSettings.Mode == CalendarMode.Json)
+            {
+                Item item;
+                if (this.Rendering.DataSourceItem == null)
+                {
+                    item = this.Context.Item;
+                }
+                else
+                {
+                    item = this.Rendering.DataSourceItem;
+                }
+                this.EventItems = (IEnumerable<Item>)item.Axes.SelectItems("//*[@@templateid = '" + Sitecore.XA.Feature.Events.Templates.CalendarEvent.ID + "']");
+            }
+
+            return calendarSettings;
+        }
+    }
+
+}
